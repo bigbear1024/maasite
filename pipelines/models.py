d9a74c723debe9b0aede420520e75a8d836875e7
bigbear1024 <bigbear1024@gmail.com>
2022-04-26 15:20:14 +0800

    修改公司名稱字數

--- pipelines/models.py
+++ pipelines/models.py
@@ -18,7 +18,7 @@ from django.urls import reverse
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=20, unique=True, verbose_name="單位")
+    name = models.CharField(max_length=100, unique=True, verbose_name="單位")
 
     def __str__(self):
         return self.name
